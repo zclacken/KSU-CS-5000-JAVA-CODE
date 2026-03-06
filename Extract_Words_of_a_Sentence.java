0c602c69ca19b03300195990af3ebd4b47d5719a
Zara Clacken <81605744+zclacken@users.noreply.github.com>
2026-03-06 00:36:39 -0500

    Add files via upload
    Java program for extracting each word of a sentence. Note - must use input.next() rather than input.nextLine()

---
+++ Extract_Words_of_a_Sentence.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class Extract_Words_of_a_Sentence
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter three words: ");
+        String word1 = input.next();
+        String word2 = input.next();
+        String word3 = input.next();
+
+        System.out.println(word1);
+        System.out.println(word2);
+        System.out.println(word3);
+    }
+}
