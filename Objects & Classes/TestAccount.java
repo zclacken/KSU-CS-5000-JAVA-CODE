e2e230c8cb41a70677eff58b2b5a81482335e414
Zara Clacken <81605744+zclacken@users.noreply.github.com>
2026-04-05 21:26:27 -0400

    Add files via upload

--- Objects & Classes/TestAccount.java
+++ Objects & Classes/TestAccount.java
@@ -7,7 +7,8 @@ public class TestAccount
     {
         Scanner input = new Scanner(System.in);
         NumberFormat money = NumberFormat.getCurrencyInstance();
-        NumberFormat percent = NumberFormat.getPercentInstance();
+        NumberFormat fmt = NumberFormat.getPercentInstance();
+        fmt.setMaximumFractionDigits(1);
 
         //Create string to prompt rerunning the program
         String rerun = "y";
@@ -31,7 +32,7 @@ public class TestAccount
             System.out.print("Enter yourAccount's Annual Interest Rate (e.g., 2.5): ");
             double yourRate = input.nextDouble();
 
-            //Create Objects
+            //Create Objects myAccount and yourAccount
             Account myAccount = new Account(myID, myBalance);
             Account yourAccount = new Account(yourID, yourBalance);
 
@@ -40,15 +41,17 @@ public class TestAccount
             yourAccount.setAnnualInterestRate(yourRate);
 
             // display myAccount information for initial deposit
-            System.out.println("\nInitial myAccount Information:");
-            System.out.println("ID: " + myAccount.getId());
-            System.out.println("Balance: " +
+            System.out.println(); //Blank Space
+            System.out.println("Initial myAccount Information:");
+            System.out.println("ID: " + "\t\t\t\t\t" + myAccount.getId());
+            System.out.println("Balance: " + "\t\t\t\t" +
                     money.format(myAccount.getBalance()));
-            System.out.println("Annual Interest Rate: " +
-                    percent.format(myAccount.getAnnualInterestRate()));
+            System.out.println("Annual Interest Rate: " + "\t" +
+                    fmt.format(myAccount.getAnnualInterestRate()));
 
             // deposit test for my account
-            System.out.print("\nEnter deposit amount for myAccount: ");
+            System.out.println();//blank space
+            System.out.print("Enter deposit amount for myAccount: ");
             double depositAmt = input.nextDouble();
 
             myAccount.deposit(depositAmt);
@@ -57,7 +60,8 @@ public class TestAccount
                     money.format(myAccount.getBalance()));
 
             // Withdraw test with overdraft check for my account
-            System.out.print("\nEnter withdrawal amount for myAccount: ");
+            System.out.println(); //blank space
+            System.out.print("Enter withdrawal amount for myAccount: ");
             double withdrawAmt = input.nextDouble();
 
             if(withdrawAmt > myAccount.getBalance())
@@ -73,36 +77,40 @@ public class TestAccount
             }
 
             // monthly interest test for my account
-            System.out.println("\nInterest Information for myAccount:");
-            System.out.println("Monthly Interest Rate: " +
-                    percent.format(myAccount.getMonthlyInterestRate()));
+            System.out.println(); //blank space
+            System.out.println("Interest Information for myAccount:");
+            System.out.println("Monthly Interest Rate: " + " \t" +
+                    fmt.format(myAccount.getMonthlyInterestRate()));
 
-            System.out.println("Monthly Interest Amount: " +
+            System.out.println("Monthly Interest Amount: " + "\t" +
                     money.format(myAccount.getMonthlyInterest()));
 
-            //----Tests for yourAccount
+            //----Tests for yourAccount------
 
             //Display initial balance information for yourAccount
-            System.out.println("\nyourAccount Initial values:");
-            System.out.println("ID:\t\t\t" + yourAccount.getId());
-            System.out.println("Balance:\t\t" +
+            System.out.println(); //blank space
+            System.out.println("yourAccount Initial values:");
+            System.out.println("ID:" + "\t\t\t\t\t\t" + yourAccount.getId());
+            System.out.println("Balance:" + "\t\t\t\t" +
                     money.format(yourAccount.getBalance()));
-            System.out.println("Annual Interest Rate:\t" +
-                    percent.format(yourAccount.getAnnualInterestRate()));
-            System.out.println("Date Created:\t\t" +
+            System.out.println("Annual Interest Rate:" + "\t" +
+                    fmt.format(yourAccount.getAnnualInterestRate()));
+            System.out.println("Date Created:" + "\t\t\t" +
                     yourAccount.getDateCreated());
 
             //Test deposit method for yourAccount
-            System.out.print("\nEnter deposit amount for yourAccount: ");
+            System.out.println(); //Blank Space
+            System.out.print("Enter deposit amount for yourAccount: ");
             double yourDeposit = input.nextDouble();
 
             yourAccount.deposit(yourDeposit);
 
-            System.out.println("Balance after deposit:\t" +
+            System.out.println("Balance after deposit:" +
                     money.format(yourAccount.getBalance()));
 
             //Test withdrawal method for yourAccount
-            System.out.print("\nEnter withdrawal amount for yourAccount: ");
+            System.out.println(); //Blank space
+            System.out.print("Enter withdrawal amount for yourAccount: ");
             double yourWithdraw = input.nextDouble();
 
             if(yourWithdraw > yourAccount.getBalance())
@@ -113,19 +121,21 @@ public class TestAccount
             {
                 yourAccount.withdraw(yourWithdraw);
 
-                System.out.println("Balance after withdrawal:\t" +
+                System.out.println("Balance after withdrawal: " +
                         money.format(yourAccount.getBalance()));
             }
 
             //Test monthly interest for yourAccount
-            System.out.println("\nMonthly Interest Rate:\t" +
-                    percent.format(yourAccount.getMonthlyInterestRate()));
+            System.out.println(); //Blank Space
+            System.out.println("Monthly Interest Rate: " + "\t\t" +
+                    fmt.format(yourAccount.getMonthlyInterestRate()));
 
-            System.out.println("Monthly Interest Amount:\t" +
+            System.out.println("Monthly Interest Amount: " + "\t" +
                     money.format(yourAccount.getMonthlyInterest()));
 
             //Sentinel loop prompt
-            System.out.print("\nRun the program again? (y/n): ");
+            System.out.println(); //Blank Space
+            System.out.print("Run the program again? (y/n): ");
             rerun = input.next();
         }
     }
